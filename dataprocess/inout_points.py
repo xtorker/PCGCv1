3f73a234f8706779a88e615150afca77c028ce1f
xtorker <xtorker@gmail.com>
2021-03-20 05:21:20 +0800

    generalize file I/O and input/output in test.py

--- dataprocess/inout_points.py
+++ dataprocess/inout_points.py
@@ -3,27 +3,17 @@
 
 import numpy as np
 import os
+from pyntcloud import PyntCloud
 
 ################### plyfile <--> points ####################
 def load_ply_data(filename):
   '''
   load data from ply file.
   '''
-  f = open(filename)
-  #1.read all points
-  points = []
-  for line in f:
-    #only x,y,z
-    wordslist = line.split(' ')
-    try:
-      x, y, z = float(wordslist[0]),float(wordslist[1]),float(wordslist[2])
-    except ValueError:
-      continue
-    points.append([x,y,z])
-  points = np.array(points)
+  cloud = PyntCloud.from_file(filename)
+  coords = ['x', 'y', 'z']
+  points = np.array(cloud.points[coords])
   points = points.astype(np.int32)#np.uint8
-  # print(filename,'\n','length:',points.shape)
-  f.close()
 
   return points
 
